3b2771e074361530224d77c011f901eb1c9a5216
Arzu Bashirova <arzubashirova17@gmail.com>
2023-03-31 12:23:59 -0400

    new commit 3/31/23

--- src/test/java/stepDefinitions/SignInStepDefinitions.java
+++ src/test/java/stepDefinitions/SignInStepDefinitions.java
@@ -7,6 +7,7 @@ import org.junit.Assert;
 import pages.SignInPage;
 import utils.ConfigReader;
 import utils.Driver;
+import utils.SeleniumUtils;
 
 public class SignInStepDefinitions {
 
@@ -28,7 +29,7 @@ public class SignInStepDefinitions {
     @When("The sign in page should have two input fields")
     public void the_sign_in_page_should_have_two_input_fields() {
 
-        Assert.assertEquals(2,new SignInPage().getInputFields().size());//code
+        Assert.assertEquals(2,new SignInPage().getInputFields().size());
 
     }
     @Then("One for email address and the other for password")
@@ -36,4 +37,72 @@ public class SignInStepDefinitions {
 
         Assert.assertEquals("Email address",new SignInPage().getEmail().getAttribute("placeholder"));
     }
+    @When("The user left the fields blank")
+    public void the_user_left_the_fields_blank() {
+
+
+    }
+    @Then("the user should not be able to submit")
+    public void the_user_should_not_be_able_to_submit_the_form() {
+        new SignInPage().clickOnSignInButton();
+        Assert.assertEquals(ConfigReader.getProperty("homepage"),Driver.getDriver().getCurrentUrl());
+    }
+
+    @When("the user left the email field blank only")
+    public void the_user_left_the_email_field_blank_only() {
+
+        SignInPage signInPage=new SignInPage();
+        signInPage.sendKeysToEmailField("");
+        signInPage.sendKeysToPasswordField("VWK@uvx2dzw5rgv1kdx");
+
+    }
+    @Then("the user should not be able to submit without email")
+    public void the_user_should_not_be_able_to_submit_the_form1() {
+        new SignInPage().clickOnSignInButton();
+        Assert.assertEquals(ConfigReader.getProperty("homepage"),Driver.getDriver().getCurrentUrl());
+    }
+    @When("the user left the password field blank only")
+    public void the_user_left_the_password_field_blank_only() {
+
+        SignInPage signInPage=new SignInPage();
+        signInPage.getPassword().clear();
+        signInPage.sendKeysToEmailField("farrokh.bulsara@gmail.com");
+        signInPage.sendKeysToPasswordField("");
+    }
+
+    @Then("the user should not be able to submit without password")
+    public void the_user_should_not_be_able_to_submit_the_form2() {
+        new SignInPage().clickOnSignInButton();
+        Assert.assertEquals(ConfigReader.getProperty("homepage"),Driver.getDriver().getCurrentUrl());
+    }
+
+    @When("the user enters an invalid email address")
+    public void the_user_enters_an_invalid_email_address() {
+
+        new SignInPage().userEnterInvalidEmail();
+    }
+    @When("clicks on the sign in button")
+    public void clicks_on_the_sign_in_button() {
+        new SignInPage().clickOnSignInButton();
+    }
+    @Then("the user should see an error message for email")
+    public void the_user_should_see_an_error_message_for_email() throws InterruptedException {
+
+//        Thread.sleep(1000);
+//        //Assert.assertEquals("Please include an '@' ");
+//        Assert.assertTrue(new SignInPage().getErrorMessage().contains("Please include an '@' "));
+//        new SignInPage().clickOnSignInButton();
+//        Assert.assertFalse(new SignInPage().errorExists(), "Verify no errors exist");
+    }
+    @Then("the sign-up process should not proceed")
+    public void the_sign_up_process_should_not_proceed() {
+
+        //Assert.assertEquals(ConfigReader.getProperty("homepage"),Driver.getDriver().getCurrentUrl());
+    }
+
+
+
+
+
+
 }
--- target/test-classes/signin.feature
+++ target/test-classes/signin.feature
@@ -6,10 +6,32 @@ Feature: As a user, I want to be able to sign in to my mortgage account, so that
 
     Given The user is on the Sign in page
 
-  Scenario: Navigate to Sign In page
+  Scenario: User navigate to Sign In page
 
     When The user navigates to the sign in page
     Then the user should be greeted with a welcome message
     Then The sign in page should have two input fields
     Then One for email address and the other for password
+
+
+  Scenario: User left email and password fields blank
+
+    When The user left the fields blank
+    Then the user should not be able to submit
+    When the user left the email field blank only
+    Then the user should not be able to submit without email
+    When the user left the password field blank only
+    Then the user should not be able to submit without password
+
+    @invalidData
+  Scenario: User enter invalid email
+
+    When the user enters an invalid email address
+    And clicks on the sign in button
+    Then the user should see an error message for email
+    And the sign-up process should not proceed
+
+
+
+
 
