eb0ead1353f9a167b901839e7b9ef3825de3bcbe
Arzu Bashirova <arzubashirova17@gmail.com>
2023-04-10 16:44:14 -0400

    sprint 2 second commit

--- src/test/java/runners/CucumberRunnerSignIn.java
+++ src/test/java/runners/CucumberRunnerSignIn.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)//sign In runner
 @CucumberOptions(
-        tags = "@containCheck",
+        tags = "@sc_outline",
         features = "src/test/resources",
         glue = "stepDefinitions",
         stepNotifications = true,
